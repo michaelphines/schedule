dd133cb30923b416e158c918c2f39fd21c9130cd
Michael Hines <mike@michaelphines.net>
2010-07-19 08:26:23 -0500

    removing old files

--- config/initializers/mongo.rb
+++
@@ -1,3 +0,0 @@
-MongoMapper.connection = Mongo::Connection.new(ENV["SCHEDULE_MONGO_HOST"], ENV["SCHEDULE_MONGO_PORT"], { :logger => Rails.logger })
-MongoMapper.database = "#{ENV["SCHEDULE_MONGO_DATABASE"]}-#{Rails.env}"
-MongoMapper.database.authenticate(ENV["SCHEDULE_MONGO_USER"], ENV["SCHEDULE_MONGO_PASSWORD"])
