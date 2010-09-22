272b0c7131c7da7e02dcba7d607ae43047002197
Michael Hines <mike@michaelphines.net>
2010-09-22 17:39:32 -0500

    Hide the preview calendar by default

--- public/javascripts/showPage.js
+++ public/javascripts/showPage.js
@@ -1,4 +1,10 @@
 $(function() {
   ScheduleButler.createCalendar('#calendarSelect', true);
   ScheduleButler.createTooltips();
+
+  $("#showPreview").click(function(event) {
+    $("#preview").show();
+    $(this).hide();
+    event.preventDefault();
+  });
 });
