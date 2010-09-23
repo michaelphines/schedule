2cf8d6be7f34f4264aa3a5d1146169d8119bc9ee
Michael Hines <mike@michaelphines.net>
2010-09-22 21:04:48 -0500

    Make calendars easier to see

--- public/javascripts/showPage.js
+++ public/javascripts/showPage.js
@@ -1,6 +1,7 @@
 $(function() {
   ScheduleButler.createCalendar('#calendarSelect', true);
   ScheduleButler.createTooltips();
+  $('#calendarSelect .next, #calendarSelect .previous').click(ScheduleButler.createTooltips);
 
   $("#showPreview").click(function(event) {
     $("#preview").show();
